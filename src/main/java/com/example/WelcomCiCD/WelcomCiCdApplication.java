7a00e7170a2abdd938ba0a142509c3347b6a4e92
unknown <anish.g.reddy@gmail.com>
2026-03-03 16:10:49 +0530

    first commit

---
+++ src/main/java/com/example/WelcomCiCD/WelcomCiCdApplication.java
@@ -0,0 +1,31 @@
+package com.example.WelcomCiCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class WelcomCiCdApplication {
+
+//	echo "# cicd" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/anish124/cicd.git
+//	git push -u origin main
+	
+	@GetMapping("/welcome")
+	public String welcome(){
+
+		return "welcome to ci/cd";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WelcomCiCD/WelcomCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WelcomCiCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
